e227e0ea4ee404cedad87284f66a278791509a72
omldgg <1527842986@qq.com>
2016-05-02 15:51:52 +0800

    360

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+}
