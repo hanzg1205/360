7cde965d82b925cfa61806e6882d2597cad01039
omldgg <1527842986@qq.com>
2016-05-04 08:36:02 +0800

    11

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ window.onload=function(){
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
-        spaceBetween: 30,
         centeredSlides: true,
         autoplay: 2500,
         autoplayDisableOnInteraction: false
